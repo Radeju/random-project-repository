dc2ef678e351c9906a1b8bace666c4e8a336c146
radoslaw jurewicz <radejur@gmail.com>
2018-02-10 18:03:22 +0100

    Initialization of the project.

---
+++ Simple Web App/PluralSightWebApp/PluralSightWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluralSightWebApp.Startup))]
+namespace PluralSightWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
